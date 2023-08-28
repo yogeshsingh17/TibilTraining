fba69ea6ebd32b0ff086b432c7224f32aff54518
yogeshsingh17 <singhyogesh492@gmail.com>
2023-08-29 00:55:32 +0530

    First Commit

---
+++ ProjectDemo/src/Project1.java
@@ -0,0 +1,18 @@
+
+public class Project1 
+{
+
+	public static void main(String[] args) 
+	{
+		int [] y= {1,2,3,4,5};
+		for(int elem: y)
+		{
+			System.out.println(elem);
+		}
+		
+		String name="Yogesh Singh";
+		int count=name.toUpperCase().length();
+		System.out.println("The length of the string is "+ count);
+	}
+
+}
